c20c1f261738d92b65d2f3bd4c9239c986573823
BAOUCHbaSalma <155727770+BAOUCHbaSalma@users.noreply.github.com>
2024-01-30 14:44:56 +0100

    Ajouter les structs

--- SAS.c
+++ SAS.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    int Jour;
+    int Mois;
 
+} Datedecheance;
+typedef struct
+{
+    int heurs;
+} HeursTaches;
+
+typedef struct
+{
+    char Nom[20];
+    char Description[50];
+    char Priorite[20];
+    char Statut[20];
+    Datedecheance Date;
+    HeursTaches Heurs;
+
+} Taches;
 
 int main(){
-    
+
     return 0;
 }
