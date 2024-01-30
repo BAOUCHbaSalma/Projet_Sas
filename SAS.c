26c10937d4710976959f18775275dc835eb5e7cb
BAOUCHbaSalma <155727770+BAOUCHbaSalma@users.noreply.github.com>
2024-01-30 14:54:50 +0100

    Ajouter le menu

--- SAS.c
+++ SAS.c
@@ -23,6 +23,51 @@ typedef struct
 
 } Taches;
 
+void MENU()
+{
+    int choice;
+    do
+    {
+        printf("\n\t\t----------Menu Principale----------\n\n");
+        printf("  1-Ajouter une Tache : \n");
+        printf("  2-Afficher la Liste des Taches :  \n");
+        printf("  3-Modifier une Tache :  \n");
+        printf("  4-Supprimer une Tache :  \n");
+        printf("  5-Ordonner les Taches:  \n");
+        printf("  6-Filtrer les Taches : \n");
+        printf("\n\n");
+        printf("Votre choix : ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            Ajouter();
+            break;
+        case 2:
+            Afficher();
+            break;
+        case 3:
+            Modifier();
+            break;
+        case 4:
+            Supprimer();
+            break;
+        case 5:
+            Ordonner();
+            break;
+        case 6:
+            Filtrer();
+            break;
+        default:
+            printf("Choix invalide. Veuillez entrer un nombre entre 1 et 6.\n");
+        }
+    } while (choice != 1);
+}
+
+int Taille = 0;
+Taches T[100];
+
 int main(){
 
     return 0;
